1475b1c873bea9d946f54d27a3cf254555c17122
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-04-08 13:48:43 +0200

    Appointments sorted by time

--- src/Day.js
+++ src/Day.js
@@ -3,7 +3,9 @@ import "./Day.css";
 import AppointmentInDay from "./AppointmentInDay";
 
 export default ({ appointments }) => {
-  const appointmentsJSX = appointments.map(
+  const appointmentsJSX = appointments.sort((a,b) => {
+    return (a.time>b.time ?  1: -1)
+  }).map(
     ({ time, patient, dentist, assistant }, index) => (
       <AppointmentInDay
         time={time}
@@ -16,3 +18,4 @@ export default ({ appointments }) => {
   );
   return <ul className="dayview">{appointmentsJSX}</ul>;
 };
+//de afspraken zijn nu gesorteerd op tijdstip
--- src/DayInMonth.js
+++ src/DayInMonth.js
@@ -1,9 +1,12 @@
 import React from "react";
 import AppointmentInMonth from "./AppointmentInMonth";
 
-export default ({ appointments }) => {
-  const appointmentsJSX = appointments.map(({ time, patient }, index) => (
-    <AppointmentInMonth time={time} patient={patient} key={index} />
+export default ({ appointments }) => { //appointments per dag
+  const appointmentsJSX = appointments.sort((a,b) => {
+    return (a.time>b.time ?  1: -1)
+  }).map(({ time, patient }, index) => (
+    <AppointmentInMonth time={time} patient={patient} key={index} />//returnt een div "appointment" voor iedere entry
   ));
   return <div className="day">{appointmentsJSX}</div>;
 };
+// de appointments zijn nu gesorteerd op tijdstip
