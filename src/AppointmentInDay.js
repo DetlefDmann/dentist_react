88037400e1e1d0ca2212112031a3bcc95184a25c
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-04-13 09:13:14 +0200

    adjusted for objects instead of just names

--- src/AppointmentInDay.js
+++ src/AppointmentInDay.js
@@ -5,9 +5,9 @@ const format_time = time => (time < 10 ? `0${time}:00u` : `${time}:00u`);
 export default ({ time, patient, dentist, assistant }) => (
   <li className="appointment">
     <div className="time">{format_time(time)}</div>
-    <div className="patient">Patiënt: {patient}</div>
-    <div className="dentist">Tandarts: {dentist}</div>
-    <div className="assistant">Assistent: {assistant}</div>
+    <div className="patient">Patiënt: {patient.name}</div>
+    <div className="dentist">Tandarts: {dentist.name}</div>
+    <div className="assistant">Assistent: {assistant.name}</div>
   </li>
 );
 // dit is hoe appointments in het dagoverzicht eruit komen te zien
