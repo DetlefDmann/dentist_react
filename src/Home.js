957d05d94a5f34232302e77888633931fc6bdda9
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-04-16 16:45:01 +0200

    simple welcome

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
+import EditAppointment from "./components/EditAppointment";
 import ScheduleNewAppointment from "./components/ScheduleNewAppointment";
 
 export default (props) => {
         return <>
-                    <ScheduleNewAppointment />
+                    <h3>Welkom in het het systeem.</h3>
+                    <p>Om een nieuwe afspraak te maken ga naar "afspraken maken/wijzgen".<br/>
+                        Voor het maandoverzicht kijkt u bij "Kalender".
+                    </p>
+
                 </>;
 }
